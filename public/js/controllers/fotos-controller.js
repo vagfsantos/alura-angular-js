4d42c1f78e0f8b7679a22b2a7bdab5ed464d6325
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-04-28 23:10:20 -0300

    ng repeat and ajax

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,10 +1,27 @@
 var model = angular.module('alurapic');
 
-model.controller('FotosController', function($scope){
+model.controller('FotosController', function($scope, $http){
 	console.log(arguments);
 	console.log($scope);
-	$scope.foto = {
-		titulo:'Leão',
-		url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	}
+	
+	$scope.fotos = [];
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(erro){
+		console.log(erro);
+	});
+	
+
+	// ajax
+	// var promise = $http.get('v1/fotos');
+	// promise.then(function(retorno){
+	// 	console.log(retorno);
+	// 	$scope.fotos = retorno.data;
+	// }).catch(function(error){
+	// 	console.log(error);
+	// });
+
 });
